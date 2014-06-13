09bb3dc3e5de20c396291ac0ea87e0654556a2f8
Loide <loide.marwanga@yale.edu>
2014-06-13 09:32:06 -0400

    Basic routes set up for Ruby file

--- essay_extender.rb
+++ essay_extender.rb
@@ -2,7 +2,18 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
 
-configure do |
-	enable :sessions|
-	
+configure do 
+	enable :sessions
+end
+
+get '/' do
+	sessions[:essays] ||= []
+	erb :index, :locals => {:essays => sessions[:essays]}
+end
+
+post '/' do  # grabbing stuff from the form
+	sessions[:essays] ||= []
+	output = params[:input].join(" ")
+	sessions[:essays].push(output)
+	erb :index, :locals => {:essays => sessions[:essays]}
 end
