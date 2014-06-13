3980b17e78a0cf73efc7b86697bcb786171dc1a2
Natalia <natalia.castillejoarias@yale.edu>
2014-06-13 11:03:22 -0400

    update

--- essay_extender.rb
+++ essay_extender.rb
@@ -1,14 +1,22 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
+require 'dinosaurus'
+
 
 configure do 
 	enable :sessions
 end
 
+Dinosaurus.configure do |config|
+	config.api_key = 'b833035f63713be4db827676274a8e96'
+end
+
+
 get '/' do
-	session[:essays] ||= []
-	erb :'index.html', :locals => {:essays => session[:essays]}
+	session[:essays] ||locals= []
+	newword = Dinosaurus.lookup('sun')
+	erb :'index.html', :locals => {:essays => session[:essays], :newword => newword}
 end
 
 post '/' do  # grabbing stuff from the form
