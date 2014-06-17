ef0da20443766b80ef42514dcd35d95b8f25f604
Loide <loide.marwanga@yale.edu>
2014-06-17 14:29:25 -0400

    Functional again

--- essay_extender.rb
+++ essay_extender.rb
@@ -13,19 +13,21 @@ Dinosaurus.configure do |config|
 	config.api_key = '98eb24a061cfcf3c760b2e8a908bebe3'
 end
 
+before do
+	session[:essays] ||= []
+end
+
 
 get '/' do
-	session[:essays] ||locals= []
 	# newword = Dinosaurus.lookup('sun')
 	erb :'index.html', :locals => {:essays => session[:essays]}
 end
 
 post '/' do  # grabbing stuff from the form
-	session[:essays] ||= []
 	output = Dinosaurus.lookup(params[:input])
 	array = session[:essays].unshift(output.synonyms)
 	specific = array[0]
-	random = specific[rand(specific.length)]
 	random ||= params[:input]
+	random = specific[rand(specific.length)]
 	erb :'index.html', :locals => {:essays => session[:essays], :random => random}
 end
