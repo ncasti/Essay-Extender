00e10dc5117d5533d1e2c48d01abc049add36e47
Natalia <natalia.castillejoarias@yale.edu>
2014-06-18 16:50:21 -0400

    adding capitalizing feature and pushing

--- essay_extender.rb
+++ essay_extender.rb
@@ -34,26 +34,46 @@ end
 
 def translate(text)
 
+
 	text_array = text.split(" ")
 
 	new_words = []
 
 	text_array.each do |word|
 		diff_words = []
-		output = Dinosaurus.lookup(word)
-		if output.synonyms == []
-			diff_words.push([word])
-			
-		else
-			diff_words.push(output.synonyms)
+		symbolselector = word.tr('A-Za-z', '') #selecting symbols in words
+		wordnosymbol = word.delete symbolselector #deletes symbols from word and saves it in a variable
+		output = Dinosaurus.lookup(wordnosymbol) #looks up the word without the symbol in the dictionary
+		if output.synonyms == [] #if the word without symbol does not exist in the thesaurus
+			pushing = word #what we are pushing
+
+		else #i.e., if the word without the symbols does exist in the dictionary
+			sy1 = output.synonyms # is the array synonyms of the word without the symbols
+			sy2 = sy1[rand(sy1.length)] #sy2 is the synonym of the word without the symbols
+			pushing = word.gsub(/#{wordnosymbol}/, sy2)
+			#diff_words.push(output.synonyms)
 		end
-		new_words.push(diff_words[0][rand(diff_words[0].length)])
+		#new_words.push(diff_words[0][rand(diff_words[0].length)])
+		new_words.push(pushing) #new thing i added
+
+		
 	end
-	new_words.join(" ")
+	s = new_words.join(" ").capitalize
+	stopA = s.enum_for(:scan, ". ").map { Regexp.last_match.begin(0) } #an array of the indexes of . /
+	#questionA = s.enum_for(:scan, "?").map { Regexp.last_match.begin(0) }
+
+
+	firstA = []
+	stopA.each do |item|
+		firstA.push(item + 2)
+	end
+	firstA.each do |index|
+		s[index] = s[index].upcase
+	end
+
+	s
 
 end
-
-# arr = ["bad.", "good.", "ugly--"]
 
 # arr.each do |word|
 # 	old_word = word
