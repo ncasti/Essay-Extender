927cdd3d946d162add327134e44d839c5fe24810
Loide <loide.marwanga@yale.edu>
2014-06-19 11:00:53 -0400

    Updated essay extender to replace words with longer synonyms.

--- essay_extender.rb
+++ essay_extender.rb
@@ -10,7 +10,7 @@ end
 
 
 Dinosaurus.configure do |config|
-	config.api_key = '98eb24a061cfcf3c760b2e8a908bebe3'
+	config.api_key = '800e8967fc19dd1f3ed95f4e43b63a78'
 end
 
 before do
@@ -20,15 +20,20 @@ end
 
 get '/' do
 	# newword = Dinosaurus.lookup('sun')
-	erb :'index.html', :locals => {:essays => session[:essays], :translation => @translation}
+	erb :'index.html', :locals => {:essays => session[:essays], 
+									:translation => @translation,
+									:input => @input}
 end
 
 
 post '/' do 
+	@input = params[:input]
 	@translation = translate(params[:input])
 	session[:essays].unshift(@translation)
 
-	erb :'index.html', :locals => {:essays => session[:essays], :translation => @translation}
+	erb :'index.html', :locals => {:essays => session[:essays], 
+									:translation => @translation,
+									:input => @input}
 end
 
 
@@ -41,36 +46,60 @@ def translate(text)
 
 	text_array.each do |word|
 		diff_words = []
+		exceptions = ["i","am","a","an","as","you","him","us","he","she","they","we","sr","jr","ms","mr","mrs","her","his","the","it","to","be","what","which","who","is","was","where","why","in","on","at","are"]
 		symbolselector = word.tr('A-Za-z', '') #selecting symbols in words
 		wordnosymbol = word.delete symbolselector #deletes symbols from word and saves it in a variable
-		output = Dinosaurus.lookup(wordnosymbol) #looks up the word without the symbol in the dictionary
-		if output.synonyms == [] #if the word without symbol does not exist in the thesaurus
-			pushing = word #what we are pushing
 
-		else #i.e., if the word without the symbols does exist in the dictionary
-			sy1 = output.synonyms # is the array synonyms of the word without the symbols
-			sy2 = sy1[rand(sy1.length)] #sy2 is the synonym of the word without the symbols
-			pushing = word.gsub(/#{wordnosymbol}/, sy2)
-			#diff_words.push(output.synonyms)
-		end
+		
+
+			output = Dinosaurus.lookup(wordnosymbol) #looks up the word without the symbol in the dictionary
+			sy1 = output.synonyms # is the array synonyms of the word without the symbolz
+			sy1.delete_if {|word| word.length < wordnosymbol.length}
+			
+			if sy1 == [] #if the word without symbol does not exist in the thesaurus
+				pushing = word #what we are pushing
+
+			else #i.e., if the word without the symbols does exist in the dictionary
+				if (exceptions.include? wordnosymbol.downcase) == true
+					pushing = word
+				elsif wordnosymbol[0] == wordnosymbol[0].upcase
+					pushing = word
+				else
+					sy2 = sy1[rand(sy1.length)] #sy2 is the synonym of the word without the symbols
+					pushing = word.gsub(/#{wordnosymbol}/, sy2)
+					#diff_words.push(output.synonyms)
+				end
+			end
 		#new_words.push(diff_words[0][rand(diff_words[0].length)])
 		new_words.push(pushing) #new thing i added
 
 		
 	end
-	s = new_words.join(" ").capitalize
+	s = new_words.join(" ")
 	stopA = s.enum_for(:scan, ". ").map { Regexp.last_match.begin(0) } #an array of the indexes of . /
-	#questionA = s.enum_for(:scan, "?").map { Regexp.last_match.begin(0) }
+	questionA = s.enum_for(:scan, "? ").map { Regexp.last_match.begin(0) }
+	exclamationA = s.enum_for(:scan, "! ").map { Regexp.last_match.begin(0) }
+
 
 
 	firstA = []
 	stopA.each do |item|
 		firstA.push(item + 2)
 	end
+
+	questionA.each do |item|
+		firstA.push(item + 2)
+	end
+
+	exclamationA.each do |item|
+		firstA.push(item + 2)
+	end
+
 	firstA.each do |index|
 		s[index] = s[index].upcase
 	end
 
+	s[0] = s[0].upcase
 	s
 
 end
